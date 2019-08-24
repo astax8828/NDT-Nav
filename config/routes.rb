52df2ff000f9a54aeea247e6aca1abb47a3d33b0
Stas8828 <astax8828@gmail.com>
2019-08-25 01:10:13 +1100

    Added views for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins, class_name: "AdminPanel::Admin"
+  # devise_for :admins, class_name: "AdminPanel::Admin"
+  devise_for :admins, class_name: "AdminPanel::Admin", controllers: {
+      :sessions => "admin_panel/admins/sessions",
+      :passwords => "admin_panel/admins/passwords",
+      :registrations => "admin_panel/admins/registrations"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
