027d027e1326f97783f61a17091b4e5907e4f151
Stas8828 <astax8828@gmail.com>
2019-08-25 02:58:12 +1100

    Added a connection of admin-admin_account.

--- app/models/admin_panel/admin.rb
+++ app/models/admin_panel/admin.rb
@@ -1,4 +1,7 @@
 class AdminPanel::Admin < ApplicationRecord
+  has_one :admin_account,
+          :class_name => 'AdminPanel::AdminAccount',
+          foreign_key: "admin_panel_admin_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/admin_panel/admin_account.rb
+++ app/models/admin_panel/admin_account.rb
@@ -1,3 +1,5 @@
 class AdminPanel::AdminAccount < ApplicationRecord
-self.table_name = 'admin_accounts'
+  #The Name of table in a databbase
+  self.table_name = 'admin_accounts'
+  belongs_to :admin_panel_admin, :class_name => 'AdminPanel::Admin'
 end
