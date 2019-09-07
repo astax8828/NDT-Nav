0aec8758d4fcfd25ce7fac44e8e453eeaa814488
Stas8828 <astax8828@gmail.com>
2019-09-07 23:50:03 +1100

    Added the column of tags in the table of articles.

--- app/controllers/admin_panel/tags_controller.rb
+++ app/controllers/admin_panel/tags_controller.rb
@@ -50,7 +50,7 @@ class AdminPanel::TagsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin_panel_tag
-      @admin_panel_tag = AdminPanel::Tag.find(params[:id])
+      @admin_panel_tag = AdminPanel::Tag.find_by(name: params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
