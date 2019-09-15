b4cbdd9bae06089b5fd2b15a047be4263b489fb3
Stas8828 <astax8828@gmail.com>
2019-09-15 16:59:52 +1100

    Added validations for the form of the new category.

--- app/models/admin_panel/category.rb
+++ app/models/admin_panel/category.rb
@@ -1,4 +1,11 @@
 class AdminPanel::Category < ApplicationRecord
+  # Name table.
   self.table_name = "categories"
+
+  # Validations.
+  validates :name, :description, presence: true
+  validates :name, length: { maximum: 255 }
+
+  # Associations for categories.
   has_many :articles, dependent: :nullify
 end
