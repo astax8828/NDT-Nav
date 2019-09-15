863b5715a63e1b3be86dd2b9b1faf743987a1d19
Stas8828 <astax8828@gmail.com>
2019-09-15 17:05:08 +1100

    Added validations for the form of the new tag.

--- app/models/admin_panel/tag.rb
+++ app/models/admin_panel/tag.rb
@@ -1,5 +1,11 @@
 class AdminPanel::Tag < ApplicationRecord
+  # Name table.
   self.table_name = 'tags'
+
+  # Validations.
+  validates :name, presence: true
+  validates :name, length: { maximum: 255 }
+
   # Associations for tags.
   has_many :taggings, dependent: :destroy
   has_many :articles, through: :taggings
